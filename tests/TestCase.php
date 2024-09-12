3db91e1ab57658de1fba77d39d8e3e97fcb9f498
Prasit Gebsaap <prasit.gebsaap@outlook.com>
2024-09-12 23:50:39 +0700

    Pinted

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,14 @@
 
 namespace Soap\WorkflowStorage\Tests;
 
-use function Orchestra\Testbench\workbench_path;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Str;
 use Orchestra\Testbench\Concerns\WithWorkbench;
-
 use Orchestra\Testbench\TestCase as Orchestra;
 use Soap\WorkflowStorage\WorkflowStorageServiceProvider;
+
+use function Orchestra\Testbench\workbench_path;
 
 class TestCase extends Orchestra
 {
@@ -30,8 +30,8 @@ class TestCase extends Orchestra
             return 'Soap\\WorkflowStorage\\Database\\Factories\\'.class_basename($modelName).'Factory';
 
         });
-        
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations'); // load the package migrations        
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations'); // load the package migrations
     }
 
     protected function getPackageProviders($app)
@@ -41,9 +41,7 @@ class TestCase extends Orchestra
         ];
     }
 
-    public function getEnvironmentSetUp($app)
-    {
-    }
+    public function getEnvironmentSetUp($app) {}
 
     /**
      * Define database migrations.
