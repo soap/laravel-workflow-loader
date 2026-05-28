9bce461466e426deb1a0c32fdf05929abae231c7
soap <1073690+soap@users.noreply.github.com>
2026-05-28 14:43:50 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -45,9 +45,9 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
         config()->set('database.connections.testing', [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => ':memory:',
-            'prefix'   => '',
+            'prefix' => '',
         ]);
     }
 
